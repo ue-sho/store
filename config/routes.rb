b8d4feaef799a899c7bcd8078fef3067c74ab6cf
ue-sho <tssu45@gmail.com>
2025-10-31 00:59:20 +0900

    Add ProductsController, view, helper, routes, and controller tests

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,15 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  # resources :products is a shortcut for the following routes:
+  #   get "/products", to: "products#index"
+  #   get "/products/new", to: "products#new"
+  #   post "/products", to: "products#create"
+  #   get "/products/:id", to: "products#show"
+  #   get "/products/:id/edit", to: "products#edit"
+  #   patch "/products/:id", to: "products#update"
+  #   put "/products/:id", to: "products#update"
+  #   delete "/products/:id", to: "products#destroy"
+  resources :products
 end
